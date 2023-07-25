445eabc8246e829fb5f2758f6a25c1849ccc7032
StuPM <stuartpmcgee@gmail.com>
2023-07-25 12:09:47 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import About from "./components/About";
 import ChartGroup from "./components/ChartGroup";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <About />
       <ChartGroup />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto max-w-screen-xl px-4 pb-6 pt-16 sm:px-6 lg:px-8">
+        <div className="mt-16 border-t border-gray-100 pt-6 sm:flex sm:items-center sm:justify-between">
+          <p className="text-center text-sm text-gray-500 sm:text-left">
+            Stuart Paul McGee &copy; 2022. All rights reserved.
+          </p>
+
+          <ul className="mt-4 flex justify-center gap-6 sm:mt-0 sm:justify-start">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/stuartpmcgee/"
+                rel="noreferrer"
+                target="_blank"
+                className="text-teal-700 transition hover:text-teal-700/75"
+              >
+                <span className="sr-only">LinkedIn</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
