65e9e81507e60af6dd5b8015f3d6f4008ca771c6
StuPM <stuartpmcgee@gmail.com>
2023-07-26 11:29:20 +0100

    Disable chart section buttons for release v1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,10 +47,10 @@ const About = () => {
             Specifiy the date range and how to group the data via the button
             below the chart.
           </p>
-          <p className="mt-4 sm:text-xl/relaxed">
+          {/* <p className="mt-4 sm:text-xl/relaxed">
             Switch between D3 and Chart JS Graph technology.
           </p>
-          <About_ChartTypes />
+          <About_ChartTypes /> */}
         </div>
       </div>
     </header>
